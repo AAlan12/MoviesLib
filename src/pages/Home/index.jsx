dc7013bf8c02206c1150375432f2b4cbdae858c3
Antonio Alan <tonio.alan07@gmail.com>
2023-04-08 11:41:50 -0300

    requisição na API de melhores filmes na page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,12 +10,25 @@ function Home() {
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
+
+    setTopMovies(data.results);
   };
   useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
 
-  }, [])
-  
-  return <div />;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className='container'>
+      <h2 className='title'>Melhores filmes:</h2>
+      <div className='movies-container'>
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+      </div>
+
+    </div>
+  );
 }
 
 export default Home;
