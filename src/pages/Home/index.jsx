d8124ad9c2280a604e735603e6bd5c51a45e73d7
Antonio Alan <tonio.alan07@gmail.com>
2023-04-08 11:25:00 -0300

    conecção com API na page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,20 @@
 import styles from './styles.module.css'
+import { useState, useEffect } from 'react';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 function Home() {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+  };
+  useEffect(() => {
+
+  }, [])
+  
   return <div />;
 }
 
