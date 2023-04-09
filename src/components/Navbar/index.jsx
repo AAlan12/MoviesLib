00f7f8a21a0f1754495e1c29097e7e75f944442e
Antonio Alan <tonio.alan07@gmail.com>
2023-04-09 10:54:27 -0300

    useNavigate no component Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import styles from './styles.module.css'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { BiSearchAlt2 } from "react-icons/bi"
 import Logo from "../../assets/objects/Logo"
 
 function Navbar() {
-    const[search, setSearch] = useState("")
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!search) return
+        navigate(`/search?q${search}`)
+        setSearch("")
+    }
+
     return (
         <nav id={styles.navbar}>
             <h2>
@@ -13,11 +23,11 @@ function Navbar() {
                     <Logo /> MoviesLib
                 </Link>
             </h2>
-            <form>
-                <input type="text" 
-                placeholder="Busque um filme" 
-                onChange={(e) => setSearch(e.target.value)} 
-                value={search}/>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    placeholder="Busque um filme"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search} />
                 <button type="submit">
                     <BiSearchAlt2 />
                 </button>
