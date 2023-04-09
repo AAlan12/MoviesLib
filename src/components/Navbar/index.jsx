7a74d3dbbab1d128f674c089dd4928b1738cdee0
Antonio Alan <tonio.alan07@gmail.com>
2023-04-09 10:38:41 -0300

    captura de valores com input usando useState

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
 import { Link } from "react-router-dom"
 import { BiSearchAlt2 } from "react-icons/bi"
 import Logo from "../../assets/objects/Logo"
 
 function Navbar() {
+    const[search, setSearch] = useState("")
     return (
         <nav id={styles.navbar}>
             <h2>
@@ -12,7 +14,10 @@ function Navbar() {
                 </Link>
             </h2>
             <form>
-                <input type="text" placeholder="Busque um filme" />
+                <input type="text" 
+                placeholder="Busque um filme" 
+                onChange={(e) => setSearch(e.target.value)} 
+                value={search}/>
                 <button type="submit">
                     <BiSearchAlt2 />
                 </button>
